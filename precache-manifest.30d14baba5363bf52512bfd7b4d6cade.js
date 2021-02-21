6156a48f50154b0057637a3069bac361723d3d22
Callum <38749032+howzitcal@users.noreply.github.com>
2021-02-21 03:32:15 -0800

    Initial commit

---
+++ precache-manifest.30d14baba5363bf52512bfd7b4d6cade.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "415d8090fc21d301d7411587c4f98c68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85de2db9475891db1f62",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eb1bbfdedfc55f4836f3",
+    "url": "/static/js/2.3fe6b38d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3fe6b38d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85de2db9475891db1f62",
+    "url": "/static/js/main.a1ec71fa.chunk.js"
+  },
+  {
+    "revision": "e34d4a325d1a42678d35",
+    "url": "/static/js/runtime-main.fa34bee5.js"
+  }
+]);
